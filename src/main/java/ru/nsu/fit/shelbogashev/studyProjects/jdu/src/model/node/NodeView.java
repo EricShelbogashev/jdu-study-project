758f8baa4d88b03139855a3c08b3fdc1f794c83d
Eric Shelbogashev <e.shelbogashev@gmail.com>
2023-03-10 02:30:53 +0700

    Fixed Annotation.

--- src/main/java/ru/nsu/fit/shelbogashev/studyProjects/jdu/src/model/node/NodeView.java
+++ src/main/java/ru/nsu/fit/shelbogashev/studyProjects/jdu/src/model/node/NodeView.java
@@ -1,7 +1,6 @@
 package ru.nsu.fit.shelbogashev.studyProjects.jdu.src.model.node;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import ru.nsu.fit.shelbogashev.studyProjects.jdu.src.model.size.Size;
 
 import java.nio.file.Path;
@@ -20,7 +19,6 @@ public interface NodeView extends Comparable<Node> {
     @NotNull
     Size size();
 
-    @Nullable
     Collection<NodeView> children();
 
     @Override
